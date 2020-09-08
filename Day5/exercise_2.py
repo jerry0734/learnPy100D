19bda6c73a6b7b443a569c6f10fa15c12c8b9563
jerryxie <jerry34@foxmail.com>
2020-09-08 13:11:57 +0800

    finished Day5 exercise

--- Day5/exercise_2.py
+++ Day5/exercise_2.py
@@ -1,3 +1,13 @@
 """
 找出10000以内的完美数
 """
+perfect_numbers = []
+for num in range(2, 10000):
+    sum = 0
+    for i in range(1, num):
+        if num % i == 0:
+            sum = sum + i
+    if sum == num:
+        perfect_numbers.append(num)
+
+print(perfect_numbers)
