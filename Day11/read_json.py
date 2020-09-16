fb67ceedacf59f0a95b3513122045ef24af86e24
jerryxie <jerry34@foxmail.com>
2020-09-16 13:52:03 +0800

    Day11 finished

--- Day11/read_json.py
+++ Day11/read_json.py
@@ -18,3 +18,23 @@
 | True / False                           | true / false |
 | None                                   | null         |
 """
+import json
+
+filename = 'Day11\\res\\json1.json'
+my_dict = {
+    'name': 'jerry',
+    'age': 3,
+    'gender': 'M',
+}
+mylist = ['123', 456]
+
+print(json.dumps(mylist))
+
+with open(filename, 'w', encoding='utf-8') as json_file:
+    json.dump(my_dict, json_file)
+    json_file.close()
+
+with open(filename, 'r', encoding='utf-8') as json_read:
+    message = json.load(json_read)
+    for k,v in message.items():
+        print(f'{k} is {v}')
